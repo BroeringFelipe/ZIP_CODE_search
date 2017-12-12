897d801b9d68f9cd7f09be2a5d6dcca1c63a5120
anabdck <ana.banderchuk@gmail.com>
2017-12-12 13:50:33 -0200

    modificacoes em main.c

--- searcher/main.c
+++ searcher/main.c
@@ -14,18 +14,32 @@ int main()
 									"RE", "RO", "RU", "SE", "SI", "SJ", "SK", "SM", "TH", "TR", "UA", "US", "UY",
 									"VA", "VI", "WF", "YT", "ZA"    };
 
-	const char endereco_arquivo[] = "../zipcodes_archives/arquivo_final.txt"; //Felipe
+	//const char endereco_arquivo[] = "../zipcodes_archives/arquivo_final.txt"; //Felipe
 	//char endereco_arquivo[] = "/Users/aninhabanderchuk/Google Drive/Engenharia Eletrônica/* 5˚ Fase */5. Programação de Computadores II/PRG: Trabalho Final - CEP/ZIP_CODE_search/zipcodes_archives/one_archive.txt"; //Ana
 
-	int n_characters, n_countries;
+	//int n_characters, n_countries;
 
-	lista_enc_t ***hash_table;
+	//lista_enc_t ***hash_table;
 
-	hash_table = create_hash_table(endereco_arquivo, &n_characters, &n_countries);
-
-
-
-    printf("Hello world!\n");
+	//hash_table = create_hash_table(endereco_arquivo, &n_characters, &n_countries);
+    /*********/
+    
+    char cidade[100];
+    char pais[3];
+    int i = 0;
+    puts("CODIGO\tPAIS\n");
+    for(i = 0; i < 83; i++){
+        printf("%d:\t%s", i + 1, nome_arquivos[i]);
+    }
+    puts("Digite o nome da cidade da qual voce deseja realizar a busca:\n");
+    scanf("%s", cidade);
+    puts("Caso voce saiba, digite o nome do pais que ela pertence:\n");
+    scanf("%s", pais);
+    
+    
+     
+     
+     /*********/
     return 0;
 
 }
